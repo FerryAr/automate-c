4c4f90d006949b000ee496fc4fd02f2cb75a5720
Azriel Akbar Ferry Ardiansyah Kusumawardhana <ferryakbarardiansyah@gmail.com>
2024-02-18 12:05:46 +0700

    up

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 // access file
 import axios from "axios";
-import { mkdir, writeFile, access } from "fs/promises";
+import { mkdir, writeFile, access, readdir } from "fs/promises";
 import { constants } from "fs";
 
 const baseUrl = "https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/";
 const baseUrlc1 = "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/pdpr/";
 
 const provinsiId = "33";
-const kabupatenId = "3306";
-const kabupaten = "Purworejo";
+const kabupatenId = "3308";
+const kabupaten = "Magelang";
 
 // make folder for kabupaten
 await mkdir(`${kabupaten}`, { recursive: true });
@@ -17,15 +17,12 @@ const fileKec = Bun.file(`${kabupatenId}.json`);
 const textKec = await fileKec.text();
 const jsonKec = JSON.parse(textKec);
 
-
 // loop through kecamatan
 for (const kec of jsonKec) {
     // make folder for kecamatan
     await mkdir(`${kabupaten}/${kec.nama}`, { recursive: true });
 
-    console.log(kec.nama);
-
-    // fetch data kelurahan from server then save to file using axios
+    // fetch data kelurahan from server
     const res = await axios.get(`${baseUrl}${provinsiId}/${kabupatenId}/${kec.kode}.json`);
     const dataKel = res.data;
 
@@ -36,16 +33,32 @@ for (const kec of jsonKec) {
         const resTps = await axios.get(`${baseUrl}${provinsiId}/${kabupatenId}/${kec.kode}/${kel.kode}.json`);
         const dataTps = resTps.data;
 
-        for (const tps of dataTps) {
-            // make folder for tps
+        // skip if tps is empty
+        if (dataTps === null) continue;
+        if (dataTps.length === 0) continue;
 
-            console.log(`GET C1 ${kabupaten} - ${kec.nama} - ${kel.nama} - ${tps.nama}`);
+        for (const tps of dataTps) {
             await mkdir(`${kabupaten}/${kec.nama}/${kel.nama}/${tps.nama}`, { recursive: true });
 
             const resc1 = await axios.get(`${baseUrlc1}${provinsiId}/${kabupatenId}/${kec.kode}/${kel.kode}/${tps.kode}.json`);
             const dataC1 = resc1.data;
 
             const dataC1Image = dataC1.images;
+
+            if (dataC1Image === null) continue;
+            if (dataC1Image.length === 0) continue;
+
+            // get length of image already exist
+            const imgExist = await readdir(`${kabupaten}/${kec.nama}/${kel.nama}/${tps.nama}`);
+            const imgExistLength = imgExist.length;
+
+            if (dataC1Image.length === imgExistLength) {
+                console.log(`SKIP: ${kabupaten} - ${kec.nama} - ${kel.nama} - ${tps.nama}`);
+                continue;
+            }
+
+            console.log(`GET C1 ${kabupaten} - ${kec.nama} - ${kel.nama} - ${tps.nama}`);
+
             for (const imgUrl of dataC1Image) {
                 if (imgUrl === null) continue;
                 if (imgUrl === "") continue;
@@ -55,19 +68,17 @@ for (const kec of jsonKec) {
                 const imgName = imgUrl.split("/").pop();
                 const imgPath = `${kabupaten}/${kec.nama}/${kel.nama}/${tps.nama}/${imgName}`;
 
-                // if image already exist, skip
-
                 try {
                     await access(`${imgPath}`, constants.F_OK);
                     console.log(`SKIP: ${imgPath}`);
                     continue;
                 } catch (error) {
+
                     console.log(`SAVE: ${imgPath}`);
                 }
 
                 writeFile(`${imgPath}`, imgBuffer);
             }
-
         }
     }
 }
